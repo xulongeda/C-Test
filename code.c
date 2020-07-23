b60bb1bfffff47c6bcaaa7074d37b4b25f4c6712
xulongeda <68681270+xulongeda@users.noreply.github.com>
2020-07-24 00:22:49 +0800

    add API for add_Func
    support a + b function

--- code.c
+++ code.c
@@ -1,7 +1,21 @@
 #include "stdio.h"
+#include "stdlib"
+
+int app_add(int a, int b)
+{
+  return (a+b);
+}
 
 int main(int argc, char *argv[])
 {
   printf("hello world!\n");
+  int a = 0;
+  int b = 0;
+  int add_result = 0;
+  a = atoi(argv[1]);
+  b = atoi(argv[2]);
+  add_resullt = app_add(a+b);  
+  printf("a + b = :%d\n", add_result);
   return 0;
 }
+
